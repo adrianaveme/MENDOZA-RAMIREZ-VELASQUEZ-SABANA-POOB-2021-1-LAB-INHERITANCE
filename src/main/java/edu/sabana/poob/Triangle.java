e86df404198b6133c6de1fb5ec8a299b2e6a3c3c
Daniela Ramírez <danielarago@users.noreply.github.com>
2021-03-24 21:51:22 -0500

    P.2 Se ponen atributos y constructores de Triangle

--- src/main/java/edu/sabana/poob/Triangle.java
+++ src/main/java/edu/sabana/poob/Triangle.java
@@ -1,4 +1,30 @@
 package edu.sabana.poob;
 
 public class Triangle extends Shape {
+    private double side1 = 1.0;
+    private double side2 = 1.0;
+    private double side3 = 1.0;
+
+    public Triangle() {
+        super ();
+    }
+
+    public Triangle(String color) {
+        super(color);
+    }
+
+    public Triangle(double side1, double side2, double side3) {
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    public Triangle(String color, double side1, double side2, double side3) {
+        super(color);
+        this.side1 = side1;
+        this.side2 = side2;
+        this.side3 = side3;
+    }
+
+    
 }
