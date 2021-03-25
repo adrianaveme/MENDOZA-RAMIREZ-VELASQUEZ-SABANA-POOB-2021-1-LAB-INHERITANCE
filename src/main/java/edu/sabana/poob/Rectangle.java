9f47df51d1dc8bebe3b521c43cfb01d54257a46d
Adriana Velásquez <adrianaveme@unisabana.edu.co>
2021-03-24 21:57:24 -0500

    P2 contructores de rectangle

--- src/main/java/edu/sabana/poob/Rectangle.java
+++ src/main/java/edu/sabana/poob/Rectangle.java
@@ -1,4 +1,26 @@
 package edu.sabana.poob;
 
 public class Rectangle extends Shape {
+
+    private double width = 1.0;
+    private double lenght = 1.0;
+
+    public Rectangle() {
+        super();
+    }
+
+    public Rectangle(String color) {
+        super(color);
+    }
+
+    public Rectangle(double width, double lenght) {
+        this.width = width;
+        this.lenght = lenght;
+    }
+
+    public Rectangle(String color, double width, double lenght) {
+        super(color);
+        this.width = width;
+        this.lenght = lenght;
+    }
 }
