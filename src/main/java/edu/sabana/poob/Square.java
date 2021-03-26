f2784fc953a0dfdf1c1192f443da0a3a4e04369f
Felipe Alejandro Mendoza <felipemena@unisabana.edu.co>
2021-03-25 20:21:38 -0500

    I P2. Arreglo Square

--- src/main/java/edu/sabana/poob/Square.java
+++ src/main/java/edu/sabana/poob/Square.java
@@ -11,18 +11,11 @@ public class Square extends Rectangle{
     }
 
     public Square(double side) {
-<<<<<<< HEAD
         super(side, side);
     }
 
     public Square(String color, double side) {
-        super(color,side,side);
-=======
-        super(side,side);
-    }
-
-    public Square(String color, double side) {
         super(color, side, side);
->>>>>>> c7ca4cc6ee9c78fa2d6baddb79d7b681bd35f849
+
     }
 }
